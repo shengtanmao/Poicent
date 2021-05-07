d055f981160fb4b02e780a82797f9375612fa827
shengtanmao <maoshengtan2011@gmail.com>
2021-05-07 18:14:49 -0400

    Formatted ml files

--- ast.ml
+++ ast.ml
@@ -1,17 +1,27 @@
 (* Abstract Syntax Tree and functions for printing it *)
 
-type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
-          And | Or
+type op =
+  | Add
+  | Sub
+  | Mult
+  | Div
+  | Equal
+  | Neq
+  | Less
+  | Leq
+  | Greater
+  | Geq
+  | And
+  | Or
 
 type uop = Neg | Not
 
-type typ = Int | Bool | Float | Void
-  | Pointer of typ
+type typ = Int | Bool | Float | Void | Pointer of typ
 
 type bind = typ * string
 
 type expr =
-    Literal of int
+  | Literal of int
   | Fliteral of string
   | BoolLit of bool
   | Id of string
@@ -25,27 +35,26 @@ type expr =
   | Noexpr
 
 type stmt =
-    Block of stmt list
+  | Block of stmt list
   | Expr of expr
   | Return of expr
   | If of expr * stmt * stmt
   | For of expr * expr * expr * stmt
   | While of expr * stmt
 
-type func_decl = {
-    typ : typ;
-    fname : string;
-    formals : bind list;
-    locals : bind list;
-    body : stmt list;
-  }
+type func_decl =
+  { typ: typ
+  ; fname: string
+  ; formals: bind list
+  ; locals: bind list
+  ; body: stmt list }
 
 type program = bind list * func_decl list
 
 (* Pretty-printing functions *)
 
 let string_of_op = function
-    Add -> "+"
+  | Add -> "+"
   | Sub -> "-"
   | Mult -> "*"
   | Div -> "/"
@@ -58,57 +67,57 @@ let string_of_op = function
   | And -> "&&"
   | Or -> "||"
 
-let string_of_uop = function
-    Neg -> "-"
-  | Not -> "!"
+let string_of_uop = function Neg -> "-" | Not -> "!"
 
 let rec string_of_expr = function
-    Literal(l) -> string_of_int l
-  | Fliteral(l) -> l
-  | BoolLit(true) -> "true"
-  | BoolLit(false) -> "false"
-  | Id(s) -> s
-  | Binop(e1, o, e2) ->
+  | Literal l -> string_of_int l
+  | Fliteral l -> l
+  | BoolLit true -> "true"
+  | BoolLit false -> "false"
+  | Id s -> s
+  | Binop (e1, o, e2) ->
       string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
-  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
-  | Assign(v, e) -> string_of_expr v ^ " = " ^ string_of_expr e
-  | Call(f, el) ->
+  | Unop (o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign (v, e) -> string_of_expr v ^ " = " ^ string_of_expr e
+  | Call (f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
-  | Subscript(e, s) -> string_of_expr e ^ "[" ^ string_of_expr s ^ "]"
-  | Refer(s) -> "&" ^ s
-  | Deref (e) -> "*" ^ string_of_expr e
+  | Subscript (e, s) -> string_of_expr e ^ "[" ^ string_of_expr s ^ "]"
+  | Refer s -> "&" ^ s
+  | Deref e -> "*" ^ string_of_expr e
   | Noexpr -> ""
 
 let rec string_of_stmt = function
-    Block(stmts) ->
+  | Block stmts ->
       "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
-  | Expr(expr) -> string_of_expr expr ^ ";\n";
-  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
-  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | For(e1, e2, e3, s) ->
-      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
-      string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+  | Expr expr -> string_of_expr expr ^ ";\n"
+  | Return expr -> "return " ^ string_of_expr expr ^ ";\n"
+  | If (e, s, Block []) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If (e, s1, s2) ->
+      "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s1 ^ "else\n"
+      ^ string_of_stmt s2
+  | For (e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1 ^ " ; " ^ string_of_expr e2 ^ " ; "
+      ^ string_of_expr e3 ^ ") " ^ string_of_stmt s
+  | While (e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
 
 let rec string_of_typ = function
-    Int -> "int"
+  | Int -> "int"
   | Bool -> "bool"
   | Float -> "float"
   | Void -> "void"
-  | Pointer(t) -> string_of_typ t ^ "*"
+  | Pointer t -> string_of_typ t ^ "*"
 
 let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
 
 let string_of_fdecl fdecl =
-  string_of_typ fdecl.typ ^ " " ^
-  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
-  ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
-  String.concat "" (List.map string_of_stmt fdecl.body) ^
-  "}\n"
+  string_of_typ fdecl.typ ^ " " ^ fdecl.fname ^ "("
+  ^ String.concat ", " (List.map snd fdecl.formals)
+  ^ ")\n{\n"
+  ^ String.concat "" (List.map string_of_vdecl fdecl.locals)
+  ^ String.concat "" (List.map string_of_stmt fdecl.body)
+  ^ "}\n"
 
 let string_of_program (vars, funcs) =
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
+  String.concat "" (List.map string_of_vdecl vars)
+  ^ "\n"
+  ^ String.concat "\n" (List.map string_of_fdecl funcs)
--- sast.ml
+++ sast.ml
@@ -3,8 +3,9 @@
 open Ast
 
 type sexpr = typ * sx
+
 and sx =
-    SLiteral of int
+  | SLiteral of int
   | SFliteral of string
   | SBoolLit of bool
   | SId of string
@@ -18,66 +19,68 @@ and sx =
   | SNoexpr
 
 type sstmt =
-    SBlock of sstmt list
+  | SBlock of sstmt list
   | SExpr of sexpr
   | SReturn of sexpr
   | SIf of sexpr * sstmt * sstmt
   | SFor of sexpr * sexpr * sexpr * sstmt
   | SWhile of sexpr * sstmt
 
-type sfunc_decl = {
-    styp : typ;
-    sfname : string;
-    sformals : bind list;
-    slocals : bind list;
-    sbody : sstmt list;
-  }
+type sfunc_decl =
+  { styp: typ
+  ; sfname: string
+  ; sformals: bind list
+  ; slocals: bind list
+  ; sbody: sstmt list }
 
 type sprogram = bind list * sfunc_decl list
 
 (* Pretty-printing functions *)
 
 let rec string_of_sexpr (t, e) =
-  "(" ^ string_of_typ t ^ " : " ^ (match e with
-    SLiteral(l) -> string_of_int l
-  | SBoolLit(true) -> "true"
-  | SBoolLit(false) -> "false"
-  | SFliteral(l) -> l
-  | SId(s) -> s
-  | SBinop(e1, o, e2) ->
-      string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
-  | SUnop(o, e) -> string_of_uop o ^ string_of_sexpr e
-  | SAssign(v, e) -> string_of_sexpr v ^ " = " ^ string_of_sexpr e
-  | SCall(f, el) ->
-      f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
-  | SSubscript(e, s) -> string_of_sexpr e ^ "[" ^ string_of_sexpr s ^ "]"
-  | SRefer(s) -> "&" ^ s
-  | SDeref(e) -> "*" ^ string_of_sexpr e
-  | SNoexpr -> ""
-				  ) ^ ")"				     
+  "(" ^ string_of_typ t ^ " : "
+  ^ ( match e with
+    | SLiteral l -> string_of_int l
+    | SBoolLit true -> "true"
+    | SBoolLit false -> "false"
+    | SFliteral l -> l
+    | SId s -> s
+    | SBinop (e1, o, e2) ->
+        string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
+    | SUnop (o, e) -> string_of_uop o ^ string_of_sexpr e
+    | SAssign (v, e) -> string_of_sexpr v ^ " = " ^ string_of_sexpr e
+    | SCall (f, el) ->
+        f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
+    | SSubscript (e, s) -> string_of_sexpr e ^ "[" ^ string_of_sexpr s ^ "]"
+    | SRefer s -> "&" ^ s
+    | SDeref e -> "*" ^ string_of_sexpr e
+    | SNoexpr -> "" )
+  ^ ")"
 
 let rec string_of_sstmt = function
-    SBlock(stmts) ->
+  | SBlock stmts ->
       "{\n" ^ String.concat "" (List.map string_of_sstmt stmts) ^ "}\n"
-  | SExpr(expr) -> string_of_sexpr expr ^ ";\n";
-  | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ ";\n";
-  | SIf(e, s, SBlock([])) ->
+  | SExpr expr -> string_of_sexpr expr ^ ";\n"
+  | SReturn expr -> "return " ^ string_of_sexpr expr ^ ";\n"
+  | SIf (e, s, SBlock []) ->
       "if (" ^ string_of_sexpr e ^ ")\n" ^ string_of_sstmt s
-  | SIf(e, s1, s2) ->  "if (" ^ string_of_sexpr e ^ ")\n" ^
-      string_of_sstmt s1 ^ "else\n" ^ string_of_sstmt s2
-  | SFor(e1, e2, e3, s) ->
-      "for (" ^ string_of_sexpr e1  ^ " ; " ^ string_of_sexpr e2 ^ " ; " ^
-      string_of_sexpr e3  ^ ") " ^ string_of_sstmt s
-  | SWhile(e, s) -> "while (" ^ string_of_sexpr e ^ ") " ^ string_of_sstmt s
+  | SIf (e, s1, s2) ->
+      "if (" ^ string_of_sexpr e ^ ")\n" ^ string_of_sstmt s1 ^ "else\n"
+      ^ string_of_sstmt s2
+  | SFor (e1, e2, e3, s) ->
+      "for (" ^ string_of_sexpr e1 ^ " ; " ^ string_of_sexpr e2 ^ " ; "
+      ^ string_of_sexpr e3 ^ ") " ^ string_of_sstmt s
+  | SWhile (e, s) -> "while (" ^ string_of_sexpr e ^ ") " ^ string_of_sstmt s
 
 let string_of_sfdecl fdecl =
-  string_of_typ fdecl.styp ^ " " ^
-  fdecl.sfname ^ "(" ^ String.concat ", " (List.map snd fdecl.sformals) ^
-  ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.slocals) ^
-  String.concat "" (List.map string_of_sstmt fdecl.sbody) ^
-  "}\n"
+  string_of_typ fdecl.styp ^ " " ^ fdecl.sfname ^ "("
+  ^ String.concat ", " (List.map snd fdecl.sformals)
+  ^ ")\n{\n"
+  ^ String.concat "" (List.map string_of_vdecl fdecl.slocals)
+  ^ String.concat "" (List.map string_of_sstmt fdecl.sbody)
+  ^ "}\n"
 
 let string_of_sprogram (vars, funcs) =
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_sfdecl funcs)
+  String.concat "" (List.map string_of_vdecl vars)
+  ^ "\n"
+  ^ String.concat "\n" (List.map string_of_sfdecl funcs)
